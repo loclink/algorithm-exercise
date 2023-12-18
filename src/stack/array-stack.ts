1d024c2401bebbd01d238429dcf6d2f46271e0d4
loclink <loclink@163.com>
2023-12-18 19:25:41 +0800

    feat: 基于数组实现栈结构

---
+++ src/stack/array-stack.ts
@@ -0,0 +1,45 @@
+/**
+ * 利用数组实现栈结构
+ * 栈结构是一个受限的线性结构
+ * 其常见的操作有:
+ * 1. push 入栈,添加一个新元素到栈顶位置
+ * 2. pop 出栈,移除栈顶元素,同时返回被移除的元素
+ * 3. peek 返回栈顶元素,不对栈内做任何修改(不会移除栈顶元素,仅返回该元素)
+ * 4. isEmpty 如果栈内没有任何元素则返回true 否则返回false
+ * 5. size 返回栈内的元素个数,也就是栈的长度与数组的length方法类似
+ */
+class ArrayStack {
+  data: any[] = [];
+  constructor(data: any[] = []) {
+    this.data = data;
+  }
+  push(element: any) {
+    this.data.push(element);
+  }
+  pop() {
+    return this.data.pop() ?? null;
+  }
+  peek() {
+    return this.data[this.data.length - 1] ?? null;
+  }
+  isEmpty() {
+    return !!this.size();
+  }
+  size() {
+    return this.data.length;
+  }
+}
+
+const stack = new ArrayStack();
+
+stack.push("aaa");
+stack.push("bbb");
+stack.push("ccc");
+
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack);
+
+export {};
